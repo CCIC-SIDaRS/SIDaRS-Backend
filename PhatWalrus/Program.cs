35c629437c99f92927a490ef2e2e264b05840dd8
Computercodes101 <computercodes101@gmail.com>
2024-01-19 14:15:49 -0700

    Move SSHBackend to module

--- PhatWalrus/Program.cs
+++ PhatWalrus/Program.cs
@@ -4,8 +4,9 @@ using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 using System.Transactions;
+using SSHBackend;
 
-namespace SSHBackend
+namespace Testing
 {
     class Testing
     {
@@ -19,52 +20,6 @@ namespace SSHBackend
             string json = File.ReadAllText(@".\SSHClients.sidars");
             Dictionary <string, string> items = JsonSerializer.Deserialize<Dictionary<string,string>>(json);
             Console.WriteLine(items["password"]);
-        }
-    }
-    class SSHConnection
-    {
-
-        // Can send a command and recieve a response to the command
-        // returns a string with the response to the command -- either the error or the result
-        public string SSHInterface(string hostaddress, string username, string password, string command)
-        {
-            using (var client = new SshClient(hostaddress, username, password))
-            {
-                client.Connect();
-                client.RunCommand(command);
-                var _command = client.CreateCommand(command);
-                _command.Execute();
-                string result = _command.Result;
-                string error = _command.Error;
-                client.Disconnect();
-                if (error != "")
-                {
-                    throw new Exception("SSH Error: " + error);
-                }else
-                {
-                    return result;
-                }
-            }
-        }
-
-        public void AddClient()
-        {
-            Dictionary<string, string> data = new();
-            Console.WriteLine("Address");
-            data["address"] = Console.ReadLine();
-            Console.WriteLine("Username");
-            data["username"] = Console.ReadLine();
-            Console.WriteLine("Password");
-            data["password"] = Console.ReadLine();
-            string json = JsonSerializer.Serialize(data);
-            File.WriteAllText(@".\SSHClients.sidars", json);
-        }
-
-        // takes in the command when the tab key is pressed and returns the most likely completed command
-        public string CiscoCommandCompletion(string currentCommand)
-        {
-
-            return "";
         }
     }
 }
--- PhatWalrus/SSHBackend.cs
+++ PhatWalrus/SSHBackend.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Renci.SshNet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SSHBackend
@@ -9,5 +11,48 @@ namespace SSHBackend
     class SSHConnection
     {
 
+        // Can send a command and recieve a response to the command
+        // returns a string with the response to the command -- either the error or the result
+        public string SSHInterface(string hostaddress, string username, string password, string command)
+        {
+            using (var client = new SshClient(hostaddress, username, password))
+            {
+                client.Connect();
+                client.RunCommand(command);
+                var _command = client.CreateCommand(command);
+                _command.Execute();
+                string result = _command.Result;
+                string error = _command.Error;
+                client.Disconnect();
+                if (error != "")
+                {
+                    throw new Exception("SSH Error: " + error);
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        public void AddClient()
+        {
+            Dictionary<string, string> data = new();
+            Console.WriteLine("Address");
+            data["address"] = Console.ReadLine();
+            Console.WriteLine("Username");
+            data["username"] = Console.ReadLine();
+            Console.WriteLine("Password");
+            data["password"] = Console.ReadLine();
+            string json = JsonSerializer.Serialize(data);
+            File.WriteAllText(@".\SSHClients.sidars", json);
+        }
+
+        // takes in the command when the tab key is pressed and returns the most likely completed command
+        public string CiscoCommandCompletion(string currentCommand)
+        {
+
+            return "";
+        }
     }
 }
