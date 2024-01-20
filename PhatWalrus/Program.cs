06e0453bfde823fe1c0daa7e0985d274920acb66
allthefood <skiergraham5280@gmail.com>
2024-01-20 12:33:53 -0700

    You can now add multiple clients to be references by name and tests are written to be test continuous ssh connections

--- PhatWalrus/Program.cs
+++ PhatWalrus/Program.cs
@@ -5,14 +5,37 @@ using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 using System.Transactions;
 using SSHBackend;
-
 namespace Testing
 {
     class Testing
     {
         private static void Main(string[] args)
         {
-            
+            SSHManager something = new SSHManager(@"C:\Users\skier\Documents\Code\SIDaRS-Backend\PhatWalrus\assests\");
+            something.AddClient();
+            //TestMultipleExecution();
+        }
+
+        // Tests the continuous execution of SSH commands using the SSH connection class
+        private static void TestMultipleExecution()
+        { 
+            Dictionary <string, Dictionary<string, string>> client = JsonSerializer.Deserialize<Dictionary<string,Dictionary<string, string>>>(File.ReadAllText(@"C:\Users\skier\Documents\Code\SIDaRS-Backend\PhatWalrus\assests\SSHClients.sidars"));
+            SSHConnection TestSwitch = new SSHConnection(client["TestSwitch"]["address"], client["TestSwitch"]["username"], client["TestSwitch"]["password"]);
+            TestSwitch.Connect();
+            while (true)
+            {
+                Console.WriteLine("Enter Command: ");
+                string command = Console.ReadLine();
+                if (command == "ur mother")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine (TestSwitch.Execute(command));
+                }
+            }
+            TestSwitch.Diconnect();
         }
     }
 }
--- PhatWalrus/SSHBackend.cs
+++ PhatWalrus/SSHBackend.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace SSHBackend
 {
@@ -14,12 +15,12 @@ namespace SSHBackend
 
         // Can send a command and recieve a response to the command
         // returns a string with the response to the command -- either the error or the result
-        public SSHConnection(string hostaddress, string username, string password, string command)
+        public SSHConnection(string hostaddress, string username, string password)
         {
             client = new SshClient(hostaddress, username, password);
         }
 
-        private void Connect()
+        public void Connect()
         {
             try
             {
@@ -30,18 +31,20 @@ namespace SSHBackend
             }
             
         }
-        private string Execute (string command)
+        public string Execute (string command)
         {
             SshCommand _command = client.CreateCommand(command);
+            Console.WriteLine("exeucting: " + _command.CommandText);
             _command.Execute();
+            Console.WriteLine("Execution Complete");
             string result = _command.Result;
             if (_command.Error != "")
             {
-                throw new Exception("SSH Command Error " + _command.Error);
+                //throw new Exception("SSH Command Error " + _command.Error);
             }
             return result;
         }
-        private void Diconnect()
+        public void Diconnect()
         {
             try
             {
@@ -64,15 +67,24 @@ namespace SSHBackend
         }
         public void AddClient()
         {
-            Dictionary<string, string> data = new();
+            Dictionary<string, Dictionary<string, string>> currentClients = new Dictionary<string, Dictionary<string, string>>();
+            try
+            {
+                currentClients = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText(assets + "SSHClients.sidars")) ?? new Dictionary<string, Dictionary<string, string>>();
+            }catch (Exception) { }
+            
+            Console.WriteLine("Device ID");
+            string ID = Console.ReadLine();
             Console.WriteLine("Address");
-            data["address"] = Console.ReadLine();
+            string address = Console.ReadLine();
             Console.WriteLine("Username");
-            data["username"] = Console.ReadLine();
+            string username = Console.ReadLine();
             Console.WriteLine("Password");
-            data["password"] = Console.ReadLine();
-            string json = JsonSerializer.Serialize(data);
-            File.WriteAllText(@".\"+assets+"SSHClients.sidars", json);
+            string password = Console.ReadLine();
+            currentClients[ID] = new Dictionary<string, string> { ["address"] = address, ["username"] = username, ["password"] = password };
+            string json = JsonSerializer.Serialize(currentClients);
+            File.WriteAllText(assets + "SSHClients.sidars", string.Empty);
+            File.WriteAllText(assets+"SSHClients.sidars", json);
         }
         // takes in the command when the tab key is pressed and returns the most likely completed command
         public string[] CiscoCommandCompletion(string[] currentCommand)
