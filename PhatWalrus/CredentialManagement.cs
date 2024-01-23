c97bd183803c2de69b713a5c77f092b3dca3da68
allthefood <skiergraham5280@gmail.com>
2024-01-23 13:44:00 -0700

    no internal classes; security is for loosers

--- PhatWalrus/CredentialManagement.cs
+++ PhatWalrus/CredentialManagement.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PhatWalrus
 {
-    internal class CredentialManagement
+     class CredentialManagement
     {
     }
 }
--- PhatWalrus/NetworkDevice.cs
+++ PhatWalrus/NetworkDevice.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PhatWalrus
 {
-    internal class NetworkDevice
+    class NetworkDevice
     {
     }
 }
