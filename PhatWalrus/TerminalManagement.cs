41d66cb4f41e0978579468e8ce5f2d48b06aa3e7
allthefood <skiergraham5280@gmail.com>
2024-01-25 15:20:47 -0700

    added send command and automatic ssh channel detection

--- PhatWalrus/TerminalManagement.cs
+++ PhatWalrus/TerminalManagement.cs
@@ -1,20 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using CredentialManager;
+using SSHBackend;
 
-namespace TerminalManager
+namespace NetworkDeviceManager
 {
+    enum ManagementProtocol 
+    {
+        SSH,
+        SSHNoExe,
+        SNMP
+    }
+
     class TerminalManager
     {
-        private string assetsFolder { get; set; }
+        private string assetsDir { get; set; }
         private Dictionary<string, object> catalystCommands { get; set; }
-        public TerminalManager(string assetsFolder) {
-            this.assetsFolder = assetsFolder;
-            this.catalystCommands = JsonSerializer.Deserialize<Dictionary<string, object>>(File.ReadAllText(assetsFolder + @"\CiscoCommandTree.json")) ?? new Dictionary<string, object>();
+        private string v4address { get; set; }
+        private ManagementProtocol protocol { get; set; }
+        private Credentials credentials { get; set; }
+        private SSHManager? sshManager { get; set; }
+
+        public TerminalManager(string assetsDir, string v4address, ManagementProtocol protocol, Credentials credentials) 
+        {
+            this.assetsDir = assetsDir;
+            this.catalystCommands = JsonSerializer.Deserialize<Dictionary<string, object>>(File.ReadAllText(this.assetsDir + @"\CiscoCommandTree.json")) ?? new Dictionary<string, object>();
+            this.v4address = v4address;
+            this.credentials = credentials;
+
+            if (protocol == ManagementProtocol.SSH)
+            {
+                sshManager = new SSHManager(this.v4address, this.credentials);
+                sshManager.Connect();
+                var task = new Task(() => { sshManager.ExecuteExecChannel("show version"); });
+                if (task.Wait(TimeSpan.FromSeconds(5)))
+                {
+                    this.protocol = ManagementProtocol.SSH;
+                } else
+                {
+                    this.protocol = ManagementProtocol.SSHNoExe;
+                }
+                sshManager.Diconnect();
+            }
         }
+
         public string[] CiscoCommandCompletion(string[] currentCommand)
         {
             IEnumerable<string> matchingValues;
@@ -26,7 +60,7 @@ namespace TerminalManager
                 {
                     for (int i = 0; i <= currentCommand.Length - 2; i++)
                     {
-                        currentCommandDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(currentCommandDictionary[currentCommand[i]].ToString());
+                        currentCommandDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(currentCommandDictionary[currentCommand[i]].ToString()) ?? new Dictionary<string, object>();
                     }
                     matchingValues = currentCommandDictionary.Keys
                                     .Where(x => x.StartsWith(currentCommand[currentCommand.Length - 1]));
@@ -47,6 +81,26 @@ namespace TerminalManager
             }
 
             return matchingValues.ToArray();
+        }
+
+        public void SendCommand(string command)
+        {
+            string response;
+            if (protocol == ManagementProtocol.SSH)
+            {
+                response = sshManager.ExecuteExecChannel(command);
+            }
+            else if (protocol == ManagementProtocol.SSHNoExe) 
+            {
+                sshManager.Connect();
+                sshManager.CreateShellStream();
+                response = sshManager.ExecuteShellStream(command);
+                sshManager.Diconnect();
+            } else
+            {
+                response = "Failed.";
+            }
+            Console.WriteLine(response);
         }
     }
 }
