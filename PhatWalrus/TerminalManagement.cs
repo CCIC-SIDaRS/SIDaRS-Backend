99e096eff34366d8b5da3e780cd492d8b20529e3
allthefood <skiergraham5280@gmail.com>
2024-01-25 12:23:09 -0700

    New namespaces

--- PhatWalrus/TerminalManagement.cs
+++ PhatWalrus/TerminalManagement.cs
@@ -1,12 +1,11 @@
-﻿using SSHBackend;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace PhatWalrus
+namespace TerminalManager
 {
     class TerminalManagement
     {
