15b088a47917dec91d5b1bd6cf897d23ebfb9043
computercodes101 <computercodes101@gmail.com>
2024-01-28 21:28:58 -0700

    Testing function for new readCallback

--- PhatWalrus/Program.cs
+++ PhatWalrus/Program.cs
@@ -6,14 +6,19 @@ using System.Security.Principal;
 using System.Transactions;
 using NetworkDeviceManager;
 using CredentialManager;
+
 namespace Testing
 {
     class Testing
     {
+        private static void ConnectionReadCallback(string output)
+        {
+            Console.WriteLine(output);
+        }
         private static void Main(string[] args)
         {
             Credentials credentials = new Credentials("walrus", "12345678!Aa", false);
-            NetworkDevice device = new NetworkDevice("Testing", "192.168.1.254", [1,2], new List<NetworkDevice>(), credentials, @".\assets");
+            NetworkDevice device = new NetworkDevice("Testing", "192.168.1.254", [1,2], new List<NetworkDevice>(), credentials, @".\assets", ConnectionReadCallback);
             device.terminal.SendCommand("Show Running-Config");
         }
     }
